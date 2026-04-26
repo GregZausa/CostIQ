5c118778664165fd3f58ec1a80a4e066574ff8d0
Greg Zausa <gregclarence.zausa@gmail.com>
2026-04-26 19:08:04 +0800

    fix: configured db.js and used connectionSting

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,15 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import pkg from "pg";
-const {Pool} = pkg;
+const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
